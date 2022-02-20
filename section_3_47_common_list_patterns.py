4b8f4a304b70e950c77657f05116acbc49c3a4d4
Ana de Barros <scralowrealow@gmail.com>
2022-02-20 17:30:54 +0000

    Section 3 - 47:  fixed the given code so that it printed a sorted list alphabetical

---
+++ section_3_47_common_list_patterns.py
@@ -0,0 +1,12 @@
+#asks you to fix this code so that it prints a sorted list of all of our friends (alphabetical)
+
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
+
+new_friend = ['Stanley']
+
+#my code starts here
+
+new_list = friends + new_friend
+new_list.sort()
+
+print(new_list)
