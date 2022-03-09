aa86ab8fb50ae83d96aebfec37d5ad9754641488
Ana de Barros <scralowrealow@gmail.com>
2022-03-09 20:43:03 +0000

    added exercise 'Pets everywhere' from section 6, 120

--- section_3_47_common_list_patterns.py
+++ section_3_47_common_list_patterns.py
@@ -1,16 +1,16 @@
-#asks you to fix this code so that it prints a sorted list of all of our friends (alphabetical)
+# asks you to fix this code so that it prints a sorted list of all of our friends (alphabetical)
 
 friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
 
 new_friend = ['Stanley']
 
-#my code starts here
+# my code starts here
 
 new_list = friends + new_friend
 new_list.sort()
 
 print(new_list)
 
-#alternative way, as given in course as example:
-#friends.extend(new_friend)
-#print(sorted(friends))
+# alternative way, as given in course as example:
+# friends.extend(new_friend)
+# print(sorted(friends))
--- sectione_4_70_tricky_counter.py
+++
@@ -1,6 +0,0 @@
-my_list = [1,2,3,4,5,6,7,8,9,10]
-counter = 0
-
-for number in my_list:
-  counter = counter + number
-print(counter)
